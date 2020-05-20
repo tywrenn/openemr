f50c19622f67be3b7126f60fd42044891e50bec9
Tyler Wrenn <wrenntd@gmail.com>
2020-05-20 02:09:32 -0400

    Test 24

--- lint_theme_style.sh
+++ lint_theme_style.sh
@@ -5,14 +5,22 @@ echo "--------------------------------------"
 npm install || failTest=true
 npx stylelint "**/*.css" || failTest=true
 npx stylelint "**/*.scss" || failTest=true
+
 if $failTest; then
-failJob=true
-mes="FAILED"
+	failJob=true
+	mes="FAILED"
+	echo "-----------------------------------------------"
+	jobTest="${mes} - Checking for PHP theme styling issues"
+	jobTests+="${jobTest}\n"
+	echo "${jobTest}"
+	echo "-----------------------------------------------"
+	exit 1
 else
-mes="PASSED"
+	mes="PASSED"
+	echo "-----------------------------------------------"
+	jobTest="${mes} - Checking for PHP theme styling issues"
+	jobTests+="${jobTest}\n"
+	echo "${jobTest}"
+	echo "-----------------------------------------------"
+	exit 0
 fi
-echo "-----------------------------------------------"
-jobTest="${mes} - Checking for PHP theme styling issues"
-jobTests+="${jobTest}\n"
-echo "${jobTest}"
-echo "-----------------------------------------------"

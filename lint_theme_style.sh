1e80b436fc36a40e74b9ea9c703e2b37cb644663
Tyler Wrenn <wrenntd@gmail.com>
2020-05-20 01:12:28 -0400

    Smoke Test 12

---
+++ lint_theme_style.sh
@@ -0,0 +1,18 @@
+failTest=false
+echo "--------------------------------------"
+echo "Checking for PHP theme styling issues"
+echo "--------------------------------------"
+npm install || failTest=true
+npx stylelint "**/*.css" || failTest=true
+npx stylelint "**/*.scss" || failTest=true
+if $failTest; then
+failJob=true
+mes="FAILED"
+else
+mes="PASSED"
+fi
+echo "-----------------------------------------------"
+jobTest="${mes} - Checking for PHP theme styling issues"
+jobTests+="${jobTest}\n"
+echo "${jobTest}"
+echo "-----------------------------------------------"
---
+++ phpcs.sh
@@ -0,0 +1,20 @@
+failTest=false
+echo "--------------------------------------"
+echo "Checking for PHP styling (PSR-12) issues"
+echo "--------------------------------------"
+composer global require "squizlabs/php_codesniffer=3.*" || failTest=true
+$HOME/.config/composer/vendor/bin/phpcs -p -n --extensions=php,inc --report-width=120 --standard=ci/phpcs.xml --report=full . || failTest=true
+# todo clean up method visibility and other refactors in src/
+# $HOME/.config/composer/vendor/bin/phpcs -p -n --extensions=php,inc --report-width=120 --standard=ci/phpcs_src.xml --report=full src/ || failTest=true
+
+if $failTest; then
+failJob=true
+mes="FAILED"
+else
+mes="PASSED"
+fi
+echo "-----------------------------------------------"
+jobTest="${mes} - Checking for PHP styling (PSR-12) issues"
+jobTests+="${jobTest}\n"
+echo "${jobTest}"
+echo "-----------------------------------------------"

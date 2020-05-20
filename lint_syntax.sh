7f15cde42f53d49814654824e2cf60140b9242a9
Tyler Wrenn <wrenntd@gmail.com>
2020-05-20 13:45:13 -0400

    Test 34

--- lint_syntax.sh
+++ lint_syntax.sh
@@ -1,21 +1,2 @@
-failTest=false
-echo "------------------------------"
-echo "Checking for PHP syntax errors"
-echo "------------------------------"
-failSyntax=false
-if find . -type f -name "*.php" -exec php -d error_reporting=32767 -l {} \; 2>&1 >&- | grep "^"; then failSyntax=true; fi
-if find . -type f -name "*.inc" -exec php -d error_reporting=32767 -l {} \; 2>&1 >&- | grep "^"; then failSyntax=true; fi
-if $failSyntax; then
-failTest=true
-fi
-if $failTest; then
-failJob=true
-mes="FAILED"
-else
-mes="PASSED"
-fi
-echo "---------------------------------------"
-jobTest="${mes} - Checking for PHP syntax errors"
-jobTests+="${jobTest}\n"
-echo "${jobTest}"
-echo "---------------------------------------"
+if find . -type f -name "*.php" -exec php -d error_reporting=32767 -l {} \; 2>&1 >&- | grep "^"; then exit 1; fi
+if find . -type f -name "*.inc" -exec php -d error_reporting=32767 -l {} \; 2>&1 >&- | grep "^"; then exit 1; fi

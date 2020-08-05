ed9da2d63e38db74b4af56bbc9f3697bee3e894b
Cliff Su <stu01509@yahoo.com.tw>
2020-08-05 20:43:42 +0800

    style: use eslint rule.

--- library/js/calendarDirectSelect.js
+++ library/js/calendarDirectSelect.js
@@ -23,47 +23,47 @@
 //
 // +------------------------------------------------------------------------------+
 function updateApptTime(marker, index, y, date, provider) {
-    row = $("#times table tr").eq(index);
-    timeSlot = row.find("td a");
-    params = timeSlot.attr("href").split("(")[1].split(",");
-    newEvtParam = params[0] + "," + params[1] + "," + params[2] + "," + date + "," + provider + "," + "0";
-    onClk = "javascript:newEvt(" + newEvtParam + ")";
+    row = $('#times table tr').eq(index);
+    timeSlot = row.find('td a');
+    params = timeSlot.attr('href').split('(')[1].split(',');
+    newEvtParam = `${params[0]},${params[1]},${params[2]},${date},${provider},0`;
+    onClk = `javascript:newEvt(${newEvtParam})`;
     marker.html(timeSlot.html());
-    marker.attr("href", onClk);
+    marker.attr('href', onClk);
 }
 
 function displayApptTime(evt) {
-    marker = $(this).find("a.apptMarker");
-    if (marker.length == 0) {
-        style = "style=\'height:" + tsHeight + ";\'";
-        $(this).find("div.calendar_day").append("<a class=\'apptMarker event event_appointment\'" + style + "></a>");
-        marker = $(this).find("a.apptMarker");
-        marker.css("z-index", 1);
+    marker = $(this).find('a.apptMarker');
+    if (marker.length === 0) {
+        style="style=\'height:"+tsHeight+";\'"
+        $(this).find('div.calendar_day').append("<a class=\'apptMarker event event_appointment\'"+style+"></a>");
+        marker = $(this).find('a.apptMarker');
+        marker.css('z-index', 1);
     }
     y = evt.pageY - $(this).offset().top;
     rem = y % tsHeightNum;
-    y = y - rem;
-    ph = $(this).find("div.providerheader");
+    y -= rem;
+    ph = $(this).find('div.providerheader');
     index = y / tsHeightNum;
-    if (ph.length == 1) {
-        y = y - ph.height();
-        if (index == 0) {
+    if (ph.length === 1) {
+        y -= ph.height();
+        if (index === 0) {
             marker.hide();
             return;
         }
     }
-    marker.css("top", y);
-    date = $(this).attr("date");
-    updateApptTime(marker, index, y, date, $(this).attr("provider"));
+    marker.css('top', y);
+    date = $(this).attr('date');
+    updateApptTime(marker, index, y, date, $(this).attr('provider'));
     marker.show();
 }
 
 function hideApptTime(evt) {
-    marker = $(this).find("a.apptMarker");
+    marker = $(this).find('a.apptMarker');
     marker.hide();
 }
 
 function setupDirectTime() {
-    $("td.schedule").mousemove(displayApptTime);
-    $("td.schedule").mouseleave(hideApptTime);
+    $('td.schedule').mousemove(displayApptTime);
+    $('td.schedule').mouseleave(hideApptTime);
 }
